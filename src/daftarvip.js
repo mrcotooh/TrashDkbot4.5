7c032f60d6126e99b826d4904e962838962d5454
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-21 13:00:57 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558494740630 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO TrashDkbot :*
+_https://chat.whatsapp.com/COKM1MB1bK0CqE8y0IDQV0GRUPO_
+}
+exports.daftarvip = daftarvip
